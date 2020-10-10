a446b5d7eda1345e46cf39e5fd9a0717a2cf4f32
Ido Azulay <idoazzz@gmail.com>
2020-10-10 15:08:09 +0300

    Started results box

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import ResultsBox from './ResultsBox'
 
-function App() {
-  return (
-    <div className="app">
-      <div className="app_container">
-        <input placeholder="Memorize a word."/>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      word: "",
+      results: {
+        first: [],
+        second: [],
+        grade: 0
+      }
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      word: event.target.value
+    });
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <div className="app_container">
+          <input onChange={this.handleChange} placeholder="Memorize a word."/>
+          <ResultsBox results={this.state.results}></ResultsBox>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
