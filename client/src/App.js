8682b4bd0e7102329197f6e04ac2da1feae47dad
Ido Azulay <idoazzz@gmail.com>
2020-10-10 14:04:03 +0300

    Added front-end structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Here will be memorize main input.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/main.py
+++ server/main.py
@@ -5,5 +5,7 @@ app = FastAPI()
 
 @app.get("/{word}")
 def associate_word(word: str):
+    if word == "":
+        return {}
     match = AssociationsMatcher(word).most_associative
     return match
