9d3568046d9abc2ca390a0adc1f998bfc67e4d86
Ido Azulay <idoazzz@gmail.com>
2020-11-25 22:05:25 +0200

    Started refactoring frontend.

--- client/src/Dictionary.js
+++ client/src/Dictionary.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import './Dictionary.scss';
 
-// TODO: Change class name.
-class Dictionary extends React.Component{
-  constructor(props){
-    super(props);
+/** Dictionary box component. */
+class Dictionary extends React.Component {
+  /**
+   * Check if the dictionary searched word equals to the main input word.
+   * @returns {boolean}
+   */
+  isSyncedToMainInput = () => {
+    return this.props.dictionary.word.toLowerCase() === this.props.word.toLowerCase();
   }
 
-  render(){
-    if(this.props.dictionary.definitions.length==0 
-      || this.props.dictionary.word !== this.props.word)
-    return <div></div>
+  /**
+   * Check if the dictionary is empty.
+   * @returns {boolean}
+   */
+  isEmptyDictionary = () => {
+    return this.props.dictionary.definitions.length == 0;
+  }
 
+  /**
+   * Get dictionary definitions div.
+   */
+  getDictionaryDefinitions = definitions => {
+    return definitions.map(
+      definition => {
+        // Definition format: `type definition_paragraph`
+        let definition_parts = definition.split(/[^A-Za-z]/);
+        const definition_type = definition_parts[0];
+        const extracted_definition = definition_parts.splice(1).join(" ");
+        return (
+          <li>
+            <span> {definition_type} </span>
+            {` ${extracted_definition}.`}
+          </li>
+        );
+      }
+    )
+  }
+
+  render = () => {
+    if(this.isEmptyDictionary() || !this.isSyncedToMainInput()){
+      return <div></div>
+    }
+    
     return <div className="definitions_container">
-       <h3>{this.props.dictionary.word}</h3>
-        <ul>
-        {
-          this.props.dictionary.definitions.map(
-            definition => {
-              let defintion_parts = definition.split(/[^A-Za-z]/); 
-              const definition_type = defintion_parts[0];
-              const extracted_definition = defintion_parts.splice(1).join(" ");
-              return <li>
-              <span className="definition_type">{definition_type}</span>
-              {` ${extracted_definition}.`} 
-              </li>;
-            }
-          )
-        }
-        </ul>
+      <h3> {this.props.dictionary.word} </h3>
+      <ul> {this.getDictionaryDefinitions(this.props.dictionary.definitions)} </ul>
     </div>
   }
 }
