905faccecb82ed758df7ea78f645f9ba4fecc620
Ido Azulay <idoazzz@gmail.com>
2020-11-26 17:54:00 +0200

    Refactored frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,30 @@
+/**
+ * Main memorizer app component.
+ */
 import './App.scss';
 import axios from 'axios';
 import React from 'react';
 import ResultsBox from './ResultsBox'
 import Dictionary from './Dictionary';
 import { AiOutlineUp } from 'react-icons/ai';
-import { AiOutlineAlert } from 'react-icons/ai';
 import { AiOutlineDown } from 'react-icons/ai';
+import { AiOutlineAlert } from 'react-icons/ai';
 
-/**
- * Main memorizer app component.
- */
-class App extends React.Component{
-  
+class App extends React.Component {
   TYPING_TIMEOUT = 1000 // miliseconds
   DEFAULT_ASSOCIATIONS_LIMIT = 10;
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    this.typingTimer = 0;
+    this.loadingNewResults = false;
+    this.activeTypingTimer = 0;
     this.state = {
       // User input word.
       word: "",
       closestWord: "",
-      
+
       results: {},
-      isLoading: false,
-      
+
       // Closest word dictionary.
       dictionary: {
         word: "",
@@ -37,82 +36,138 @@ class App extends React.Component{
       showingAdvancedSearch: true,
       associationsLimit: this.DEFAULT_ASSOCIATIONS_LIMIT,
     };
-    
+
     // Cancel token for pending requests.
     this.current_word_requests_cancel_token = axios.CancelToken.source();
   }
 
+  /**
+   * Handle changes in the main input and dispatch the typing timer.
+   * @param {Event} event Onchange event. 
+   * Each time the timer is finishing it calculates the closest word hint.
+   */
   handleMainInputChange = event => {
     let currentWord = event.target.value;
-    // Cancel token for request 
-    if(this.state.isLoading){
+
+    // Cancel token for current request and generate new one. 
+    if (this.loadingNewResults) {
       this.current_word_requests_cancel_token.cancel();
       this.current_word_requests_cancel_token = axios.CancelToken.source();
     }
+
     this.setState({
       word: currentWord,
       closestWord: ""
     });
-    
-    if(currentWord === ""){
+
+    if (currentWord === "") {
       return;
     }
 
-    if(this.typingTimer){
-      clearTimeout(this.typingTimer);
+    if (this.activeTypingTimer) {
+      clearTimeout(this.activeTypingTimer);
     }
 
-    this.typingTimer = setTimeout(() => {
+    this.activeTypingTimer = setTimeout(() => {
       this.findClosestWord(currentWord);
     }, this.TYPING_TIMEOUT);
   }
 
+  /**
+   * Check if the given word equals to the main input word.
+   * @returns {boolean}
+   */
+  isSyncedToMainInput = word => {
+    return word.toLowerCase() === this.state.word.toLowerCase()
+  }
+
+  /**
+   * Check if the given string is only alpha letters.
+   * @param {string} string Target string. 
+   */
+  isAllLetters = (string) => {
+    let letters = /^[A-Za-z]+$/;
+    if (string.match(letters))
+      return true;
+    return false;
+  }
+
+  /**
+   * Find the closest word (input error correction).
+   * @param {word} word Target word.
+   */
   findClosestWord = word => {
-    axios.get(`/closest/${word}`,{
+    axios.get(`/closest/${word}`, {
       cancelToken: this.current_word_requests_cancel_token.token,
     }).
-    then(response => {
-      if(word === this.state.word){
-        // If the current input word is the searched word.
-        console.log(response.data.word)
-        this.setState({
-          closestWord: response.data.word,
-        });
-      }
-    }).catch(() => {});
+      then(response => {
+        if (this.isSyncedToMainInput(word)) {
+          this.setState({
+            closestWord: response.data.word,
+          });
+        }
+      }).catch(() => { });
   }
 
+  /**
+   * Associations limit changes handler.
+   * @param {Event} event onChange event. 
+   */
   handleAssociationsLimitChange = event => {
-    this.setState({associationsLimit: event.target.value});
+    this.setState(
+      {
+        associationsLimit: event.target.value,
+      }
+    );
   }
 
-  getErrorBox = () => {
-    if(!this.isAllLetters(this.state.word) && this.state.word){
-      return <div className="helper error"> 
-            <AiOutlineAlert className=""/> We don't like associate stuff that are not letters...
-              </div>; 
-    }
+  /**
+   * Auto splitting checkbox changes handler.
+   * @param {Event} event onChange event. 
+   */
+  handleAutosplittingCheck = () => {
+    this.setState(
+      {
+        autoSplitting: !this.state.autoSplitting
+      }
+    );
   }
 
-  isAllLetters = (string) => {
-   let letters = /^[A-Za-z]+$/;
-   if(string.match(letters))
-      return true;
-   return false;
-  }
-
+  /**
+   * Submitting word form for generating associations. 
+   * @param {Event} event onSubmit event. 
+   */
   handleSubmit = event => {
     event.preventDefault()
     let searchedWord = this.state.word.toLowerCase();
-    if(!this.isAllLetters(searchedWord))
-      return;
-
-    this.fetchWordDefinition(searchedWord);
-    this.fetchWordAssociations(searchedWord);
+    this.performSearch(searchedWord);
   }
 
-  fetchWordAssociations = word => {
-    this.setState({isLoading: true});
+  /**
+   * Perform total search and update results for specific word.
+   * @param {string} word Searched word. 
+   */
+  performSearch = word => {
+    if (!this.isAllLetters(word))
+      return;
+
+    this.updateWordDefinition(word);
+    this.updateWordAssociations(word);
+  }
+
+  /**
+   * @param {boolean} loading Is the component loading results. 
+   */
+  updateLoading = loading => {
+    this.loadingNewResults = loading;
+  }
+
+  /**
+   * Get from the server the compatible associations for this word.
+   * @param {string} word Searched word. 
+   */
+  updateWordAssociations = word => {
+    this.updateLoading(true);
     axios.get(`/associations/${word}`, {
       cancelToken: this.current_word_requests_cancel_token.token,
       params: {
@@ -120,101 +175,124 @@ class App extends React.Component{
         limit: this.state.associationsLimit
       }
     }).
-    then(response => {
-      this.setState({
-        isLoading: false,
-        results: response.data,
-      });
-    }).catch(() => {});
+      then(response => {
+        this.updateLoading(false);
+        this.setState({ results: response.data });
+      }).catch(() => { });
   }
 
-  fetchWordDefinition = word => {
+  /**
+   * Searching and updating word definition.
+   * @param {string} word Searched word.
+   */
+  updateWordDefinition = word => {
     axios.get(`/definitions/${word}`, {
       cancelToken: this.current_word_requests_cancel_token.token,
     }).
-    then(response => {
-      this.setState({
-        dictionary: {
-          word: response.data.word,
-          definitions: response.data.definitions,
-        }});
-    }).catch(() => {});
+      then(response => {
+        this.setState({
+          dictionary: {
+            word: response.data.word,
+            definitions: response.data.definitions,
+          }
+        });
+      }).catch(() => { });
   }
-  
+
+  /**
+   * Check if the word is valid, if not return an error box.
+   */
+  getErrorBox = () => {
+    if (!this.isAllLetters(this.state.word) && this.state.word) {
+      return (
+        <div className="helper error">
+          <AiOutlineAlert className="" /> We don't like associate stuff that are not letters...
+        </div>
+      );
+    }
+  }
+
+  /**
+   * Check if there is word connection, if exists return correction box.
+   */
   getWordCorrectionBox = () => {
-    if(this.state.closestWord !== this.state.word.toLowerCase() &&
-       this.state.closestWord !== ""){
-      return <div className="helper" 
-                  onClick={() => {
-                      this.setState({word: this.state.closestWord});
-              }}>
-        Did you mean <i>{this.state.closestWord}</i>?
-      </div>;
-    }  
+    const closestWord = this.state.closestWord;
+    if (!this.isSyncedToMainInput(closestWord) && closestWord !== "") {
+      return (
+        <div className="helper" onClick={() => {
+          // Correct the current word and perform new search.
+          const newWord = closestWord;
+          this.setState({ word: newWord });
+          this.performSearch(newWord);
+        }}>
+        Did you mean <i>{closestWord}</i>?
+        </div>
+      );
+    }
     return <div></div>;
   }
 
-  handleAutosplittingCheck = () => {
-    this.setState({ autoSplitting: !this.state.autoSplitting })
+  /**
+   * Toggle advanced search display.
+   */
+  toggleAdvancedSearch = () => {
+    this.setState(
+      { 
+        showingAdvancedSearch: !this.state.showingAdvancedSearch 
+      }
+    );
   }
-  
+
+  /**
+   * Get advanced search box.
+   */
   getAdvancedSearchBox = () => {
     const showingAdvancedSearch = this.state.showingAdvancedSearch;
-    if(!showingAdvancedSearch)
+    if (!showingAdvancedSearch)
       return (
-      <div>
-      <AiOutlineDown
-        onClick={ () => { 
-          this.setState({showingAdvancedSearch: !showingAdvancedSearch}) 
-        }}
-        className="search_settings_button"/>
-      </div>
-      )
-    else{
+        <div>
+          <AiOutlineDown onClick={this.toggleAdvancedSearch} className="search_settings_button"/>
+        </div>
+      );
+    else {
       return (
-      <div className="advanced_search">
-        <AiOutlineUp className="search_settings_button"
-            onClick={ () => { 
-              this.setState({showingAdvancedSearch: !showingAdvancedSearch}) 
-            }}
-        />
-        <span className="input_header">Associations</span>
-        <input placeholder="?" onChange={this.handleAssociationsLimitChange} 
-              value={this.state.associationsLimit}/>
+        <div className="advanced_search">
+          <AiOutlineUp onClick={this.toggleAdvancedSearch} className="search_settings_button"/>
+          <span className="input_header">Associations</span>
+          <input onChange={this.handleAssociationsLimitChange} placeholder="?" value={this.state.associationsLimit}/>
 
-        <span className="input_header"> Autosplitting</span>
-        <input className="checkbox" checked={this.state.autoSplitting} 
-                onChange={this.handleAutosplittingCheck} type="checkbox"/>
-      </div>
-     )
+          <span className="input_header"> Autosplitting</span>
+          <input onChange={this.handleAutosplittingCheck} className="checkbox" checked={this.state.autoSplitting} 
+                 type="checkbox"/>
+        </div>
+      );
     }
   }
-  
-  render(){
-    let loading = this.state.isLoading;
-    let button_class = loading ? "rotating" : "";
 
+  render() {
     return (
       <div className="app_container">
         <div className="main_form">
           <form onSubmit={this.handleSubmit}>
-          { this.getErrorBox() }
-            <input className="main_input" onChange={this.handleMainInputChange} 
-                   placeholder="Memorize a word." value={this.state.word}/>
+            {this.getErrorBox()}
+
+            <input className="main_input" onChange={this.handleMainInputChange}
+              placeholder="Memorize a word." value={this.state.word} />
+
             <button className="main_input submit_button" type="submit">
-              <AiOutlineAlert className={button_class}/>
+              <AiOutlineAlert className={this.loadingNewResults ? "rotating" : ""} />
             </button>
           </form>
-          { this.getWordCorrectionBox() }
-          { this.getAdvancedSearchBox() }
-          <Dictionary dictionary={this.state.dictionary} word={this.state.word}/>
-        </div>
-        <div className="results_container">
-          <ResultsBox word={this.state.word}
-                      results={this.state.results}
-                      isLoading={this.state.isLoading}/>
-        </div>
 
+          {this.getWordCorrectionBox()}
+          {this.getAdvancedSearchBox()}
+
+          <Dictionary dictionary={this.state.dictionary} word={this.state.word} />
+        </div>
+        
+        <div className="results_container">
+          <ResultsBox word={this.state.word} results={this.state.results} loadingNewResults={this.loadingNewResults} />
+        </div>
       </div>
     );
   }
--- client/src/ResultsBox.js
+++ client/src/ResultsBox.js
@@ -1,3 +1,6 @@
+/**  
+ * Associations results box componenet.
+ */
 import React from 'react';
 import './ResultsBox.scss';
 import logo from './assets/logo.png';
@@ -10,60 +13,75 @@ class ResultsBox extends React.Component{
     }
   }
 
+  /**
+   * Is the given word is already highlighted (list).
+   * @param {string} word Clicked word. 
+   */
+  isAlreadyHighlighted = word => {
+    return this.state.highlights.includes(word);
+  }
+
+  /***
+   * Add clicked word into marked list.
+   */
   handleWordClick = event => {
+    const word = event.target.text;
     let highlights = this.state.highlights;
-    if(!this.state.highlights.includes(event.target.text))  
-      highlights.push(event.target.text);
-  
-    else{
-      const index = this.state.highlights.indexOf(event.target.text);
-      if (index > -1) {
-        highlights.splice(index, 1);
-      }
+
+    if(!this.isAlreadyHighlighted(word))  
+      highlights.push(word);
+
+      else{
+      const index = this.state.highlights.indexOf(word);
+      highlights.splice(index, 1);
     }
 
     this.setState({highlights})
   }
   
-  getSplitResults = (index) => {
-    if(this.props.results.splits.length <= index)
-      return; 
-
-    console.dir(this.props);
-
-    let associations_list = this.props.results.splits[index].associations.map(result => {
+  /**
+   * Get specific split results.
+   * @param {int} index Split number. 
+   */
+  getResultsSection = (results) => {
+    let associationsList = results.associations.map(result => {
       let className = "association";
-      if(this.state.highlights.includes(result.name))
+      if(this.isAlreadyHighlighted(result.name))
         className += " mark"
-      return <a className={className} href="#" onClick={this.handleWordClick}>{result.name}</a>;
+      return <a className={className} href="#" onClick={this.handleWordClick}> {result.name} </a>;
     });
-    
     return (
-    <div>
-      <header className="word_split">{this.props.results.splits[index].word}</header>
-      <div className="associations_list">
-        {associations_list}
+      <div>
+        <header className="word_split">{results.word}</header>
+        <div className="associations_list">
+          {associationsList}
+        </div>
+        <br/>
       </div>
-      <br/>
-    </div>
     );
   }
   
   render(){
-    let loading = this.props.isLoading;
-    let box_class = loading ? "box_container loading" : "box_container";
-    if(!this.props.results || 
-        Object.keys(this.props.results).length === 0)
-        return <div className={box_class}>
+    let box_class = this.props.loadingNewResults ? "box_container loading" : "box_container";
+    if(!this.props.results || Object.keys(this.props.results).length === 0){
+      return (
+        <div className={box_class}>
           <a>
-              <img src={logo} className="logo"/>
-              <br/>
+            <img src={logo} className="logo"/>
+            <br/>
           </a>
-        </div>;
-    return <div className={box_class}>
-        {this.getSplitResults(0)} 
-        {this.getSplitResults(1)}
-      </div>;
+        </div>
+      );
+    }
+    return (
+      <div className={box_class}>
+        {
+          this.props.results.splits.map((element)=>{
+            return this.getResultsSection(element)
+          })
+        } 
+      </div>
+    );
   }
 }
 
